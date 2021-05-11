c50b0998aab392c257447901d5ec889051cb7c66
alescofet <alex.escofet96@gmail.com>
2021-05-11 12:37:44 +0200

    Backend connected to mongoAtlas

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7osla.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
+.connect(process.env.DB_URL , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
--- models/Hobbie.model.js
+++ models/Hobbie.model.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema
 const hobbieSchema = new Schema(
   {
     name: { type: String, required: true },
-    foto: { type: String, required: true },
+    photo: { type: String, required: true },
     description:{type:String},
     users: [{type: Schema.Types.ObjectId, ref: 'User'}]
   },
